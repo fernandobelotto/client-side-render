e197f7be0b0e95457ad70eab3d9749c371d75da6
Fernando Belotto <fernando.bbosco@gmail.com>
2023-07-15 19:05:47 +0100

    initial

---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const user1 = {
+    id: 'asdf',
+    name: 'John',
+    age: 25,
+}
+
+const user2 = {
+    id: 'asdfg',
+    name: 'Mark',
+    age: 35,
+}
+
+const users  = {
+    'asdf' : user1,
+    'asdfg' : user2
+}
+
+app.get('/health', (req, res) => {
+    res.json({
+        status: 'up',
+    })
+    }
+)
+
+app.get('/users', (req, res) => {
+    res.json({
+        users: Object.values(users),
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    res.json({
+        user: users[id]
+    })
+
+})
+
+app.put('/users/:id', (req, res) => {
+
+})
+app.delete('/users/:id', (req, res) => {
+
+})
+app.post('/users', (req, res) => {
+
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
